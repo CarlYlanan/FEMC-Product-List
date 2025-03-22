b32387e464c4daf67b5b2e4d97904dc98f926667
CarlYlanan <cylanan05@gmail.com>
2025-03-22 20:33:30 +1300

    Feat: Added dessert Card

--- src/Add_to_cart.tsx
+++ src/Add_to_cart.tsx
@@ -1,25 +1,44 @@
 import { useState } from "react";
 
 function Button({ text }: { text: string }) {
-  const [isHovered, setHovered] = useState(false);
-  const handleMouse = () => setHovered(!isHovered);
   const [count, setCount] = useState(0);
 
   function handleClick() {
     setCount(count + 1);
   }
+  
+  function handleClickDecrease() {
+    if (count > 0) {
+      setCount(count - 1)
+    };
+  }
+
+
 
   return (
     <button
-      onClick={handleClick}
       className={`w-40 h-11 absolute top-[68%] left-1/2 transform -translate-x-1/2 -translate-y-1/4  p-4 rounded flex justify-center items-center ${
-        isHovered ? "bg-[#C83A10]" : "bg-white"
+        (count > 0) ? "bg-[#C83A10]" : "bg-white"
       } rounded-xl text-black`}
-      onMouseEnter={handleMouse}
-      onMouseLeave={handleMouse}
+
     >
+      <div className="flex justify-between w-full ">
+
+      <button onClick={handleClickDecrease}>
+        <img src="\assets\images\icon-decrement-quantity.svg"/>
+      </button>
+
       <img src="\assets\images\icon-add-to-cart.svg" />
-      {count}
+      <p>{count ?  `${count}` : 'Add to Cart'}</p>
+
+        
+      <button onClick={handleClick}>
+        <img src="\assets\images\icon-increment-quantity.svg" />
+
+      </button>
+
+</div>
+
     </button>
   );
 }
